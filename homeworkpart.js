44bcf5b62c640f6efc65aeee66e6837d00fd98db
BenSears9 <Beb.brewster.sears@gmail.com>
2020-01-30 15:55:33 -0500

    Homework Jan30

--- homeworkpart.js
+++ homeworkpart.js
@@ -11,3 +11,38 @@ console.log(num + 2);
 console.log(2 + 2);
 
 console.log(2 + true);
+
+/* Conditional */
+
+counter = 2;
+counter += 5;
+counter *= 2;
+console.log(counter);
+counter %= 3;  
+//2
+counter %= 2
+console.log(counter);
+//yes
+
+if(counter == 0){
+    console.log("answer is true")
+} else {
+    console.log("variable is not even")
+}
+
+if(counter > -1 && counter < -200){
+    console.log("if this appears Good")
+} 
+if(counter > 3 || counter < -3){
+    console.log("if this appears that is bad")
+}
+
+if(counter > 3);
+    if(counter < 5);
+        if(counter = false); {
+            console.log("This hopefully shows")
+        }
+//multiple if statements are back to back, a conditional statement is an if statement
+
+var counter;
+var counter + 1 
