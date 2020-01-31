19397fea8f6c1c1084d2ca96a872b493f1bdae2a
BenSears9 <Beb.brewster.sears@gmail.com>
2020-01-31 16:11:35 -0500

    friday

--- homeworkpart.js
+++ homeworkpart.js
@@ -42,7 +42,67 @@ if(counter > 3);
         if(counter = false); {
             console.log("This hopefully shows")
         }
-//multiple if statements are back to back, a conditional statement is an if statement
+//multiple if statements are written back to back, a conditional statement is an if statement 
+//so multiple if statements are conditional statements, i think...
 
-var counter;
-var counter + 1 
+// var counter;
+// var counter + 1; 
+
+var j = 0;
+for (var i =0; i <= 50; i++){
+    console.log(i);
+} 
+
+while(j <= 50){
+    if(j %2 === 0){
+        console.log(j);
+    }
+    j++;
+}
+
+// Loop 4 fizzbuzz
+var a;
+for (a = 0; a <= 100; a++){
+    console.log(a)
+    if(a %3 === 0 && a %5 === 0)
+        console.log("fizzbuzz")
+        else if(a %3 === 0)
+        console.log("fizz")
+        else if(a %5 === 0)
+        console.log("buzz")
+}
+
+// Function homework
+
+function buzz (x,y){
+    console.log(x+y)
+}
+buzz(7,11);
+
+function fizz (x,y){
+    console.log(x*y)
+}
+fizz(2,3);
+
+var i = 1
+
+while (i <= 50){
+    if(i %2===0){
+        console.log(i);
+    }
+    i++;
+}
+// #4
+
+var x;
+function great(j){
+    console.log(j+x)
+}
+great(j,x)
+
+//5
+
+Function(success){
+    express
+
+}
